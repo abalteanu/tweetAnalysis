8f382bae2cf3e1f880ce46bbedf48d5e28ae766f
abalteanu <58431085+abalteanu@users.noreply.github.com>
2021-11-17 20:37:20 -0500

    Final code with comments
    added comments and fixed bugs. Made output looked good

--- Assign3/main.py
+++ Assign3/main.py
@@ -1,12 +1,56 @@
-import sentiment_analysis
+# Ana Balteanu
+# Computer Science 1026
+# Professor Bryan Sarlo
+# November 17, 2021
 
+
+'''Assignment 3 - Sentiment Analysis Program'''
+# This program analyses tweets based on keywords and keyword values, as well as the location of the tweet in order to
+# determine an overall happiness score in each area: Eastern, Central, Mountain, Pacific. In this program, a series of
+# functions determine the region of each tweet, separate different aspects of the keyword and tweets file, and determine
+# the average happiness score.
+# The program outputs the average happiness scrore, the tweets, and the tweets containing keywords for each region.
+
+
+import sentiment_analysis
 
 def main():
 
-    tweetsFileName = "tweets_Ana.txt"
-    keyFileName = "keywords_Ana.txt"
+    # asking user for input
+    key_file_name = input("Enter keyword file name: ")
+    tweets_file_name = input("Please enter the tweets file: ")
 
-    print(sentiment_analysis.compute_tweets(tweetsFileName, keyFileName))
+    tweet_analysis = sentiment_analysis.compute_tweets(tweets_file_name, key_file_name)
+
+    '''OUTPUT'''
+    if not tweet_analysis:
+        # if compute_tweets goes through try/except statement and cannot open file, it returns an empty list
+        print("Error: empty list returned")
+    else:
+        print("\nEASTERN")
+        print("Average happiness score: %.2f" % round(tweet_analysis[0][0], 2))
+        print("Tweets in this region containing a keyword: %.2f" % round(tweet_analysis[0][1], 2))
+        print("Number of tweets in this region: %.2f"% round(tweet_analysis[0][2], 2))
+
+
+        print("\nCENTRAL")
+        print("Average happiness score: %.2f" % round(tweet_analysis[1][0], 2))
+        print("Tweets in this region a keyword: %.2f" % round(tweet_analysis[1][1], 2))
+        print("Number of tweets in this region: %.2f"% round(tweet_analysis[1][2], 2))
+
+        print("\nMOUNTAIN")
+        print("Average happiness score: %.2f" % round(tweet_analysis[2][0], 2))
+        print("Tweets in this region a keyword: %.2f" % round(tweet_analysis[2][1], 2))
+        print("Number of tweets in this region: %.2f"% round(tweet_analysis[2][2], 2))
+
+
+
+        print("\nPACIFIC")
+        print("Average happiness score: %.2f" % round(tweet_analysis[3][0], 2))
+        print("Tweets in this region a keyword: %.2f" % round(tweet_analysis[3][1], 2))
+        print("Number of tweets in this region: %.2f"% round(tweet_analysis[3][2], 2))
+
+
 
 
 if __name__ == "__main__":
--- Assign3/sentiment_analysis.py
+++ Assign3/sentiment_analysis.py
@@ -1,4 +1,4 @@
-'''Function File'''
+"""Function File"""
 
 '''CONSTANTS'''
 # coordinate constants for region ranges
@@ -12,52 +12,46 @@ P7 = (49.189787, -115.236428)
 P8 = (24.660845, -115.236428)
 P9 = (49.189787, -125.242264)
 P10 = (24.660845, -125.242264)
-PUNC = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
+
+PUNC = '''!()-[]{};:'"\,<>./?@#$%^&*_~\"\''''
 
 
 def compute_tweets(f_tweets_name, f_key_name):
+    """Main function for computing the tweets"""
+
     end_list = []  # final list to return
     try:
-        fTweets = open(f_tweets_name, encoding='utf-8', errors='ignore')
-        fKey = open(f_key_name, encoding='utf-8', errors='ignore')
+        f_Tweets = open(f_tweets_name, encoding='utf-8', errors='ignore')
+        f_Key = open(f_key_name, encoding='utf-8', errors='ignore')
     except IOError:
         # if one of the files does not exist, return an empty list
         return end_list
 
+    '''Organizing info from f_Key'''
+    key_list = organizeKeywords(f_Key)  # key_list is a list of tuples containing a keyword followed by its score
 
-
-    '''Organizing info from fKey'''
-    key_list = organizeKeywords(fKey) #key_list is a list of touples containing a keyword followed by its score
-
-    '''Organizing info from fTweets'''
-    coordinates_list = []
-    tweet_list = []
+    '''Organizing info from f_Tweets'''
+    coordinates_list = []  # list of tuples containing coordinates for each tweet
+    tweet_list = []  # list of tweets
     # list that will have four elements, each which will count the number of points in a certain region (E, C, M, P)
     count_of_tweets = [0, 0, 0, 0]
-    region_score = [0, 0, 0, 0] # for the average happiness score of each region
-
-    for item in fTweets:
-
+    region_score = [0, 0, 0, 0]  # for the average happiness score of each region
+    for item in f_Tweets:
         # iterates through each line in the function
-        tweet_info_list = item.rstrip("\n").split(" ")
+        tweet_info_list = item.rstrip("\n").split(" ")  # line to process taken directly from tweets file
         tempCoordinate = (tweet_info_list[0].strip(",[]"), tweet_info_list[1].strip(",[]"))  # makes coordinate tuple
 
         coordinates_list.append(tempCoordinate)  # makes a list of tuples (list of coordinates)
         temp_tweet = []  # temporary list containing a list of the individual words in the tweet
 
-        # print(key_list)
         for j in range(5, len(tweet_info_list)):
             # populating list by iterating through each item in original list, starting after unnecessary info
             temp_tweet.append(tweet_info_list[j].lstrip(PUNC).rstrip(PUNC).lower())
-        # adds list of words in individual tweet to a list of tweets
-        tweet_list.append(temp_tweet)
 
-        #print(temp_tweet)
+        tweet_list.append(temp_tweet)  # adds list of words in individual tweet to a list of tweets
     # counting tweets in each region
     for c in coordinates_list:
         tweetLocation(c, count_of_tweets)
-    #print(count_of_tweets)
-
 
     '''Calculating score'''
     count_of_keyword_tweets = calculateTweetScore(tweet_list, key_list, coordinates_list, region_score)
@@ -65,7 +59,7 @@ def compute_tweets(f_tweets_name, f_key_name):
     # calculating the average happiness score for each region
     for i in range(len(region_score)):
         if count_of_keyword_tweets[i] != 0:
-            region_score[i] = region_score[i]/count_of_keyword_tweets[i]
+            region_score[i] = region_score[i] / count_of_keyword_tweets[i]
     # print(region_score, count_of_keyword_tweets, count_of_tweets)
 
     east_tuple = (region_score[0], count_of_keyword_tweets[0], count_of_tweets[0])
@@ -74,11 +68,11 @@ def compute_tweets(f_tweets_name, f_key_name):
     pacific_tuple = (region_score[3], count_of_keyword_tweets[3], count_of_tweets[3])
 
     end_list = [east_tuple, central_tuple, mountain_tuple, pacific_tuple]
-    return(end_list)
+    return (end_list)
 
-'''Organizing info from fKey'''
+
 def organizeKeywords(file):
-
+    """Organizing info from fKey"""
     kList = []
     for item in file:
         # iterating through each keyword and its value, creating a list for each pair
@@ -90,66 +84,58 @@ def organizeKeywords(file):
         kList.append(temp)
     return kList
 
-def calculateTweetScore(sentenceList, keywordList, coordinates, regionScoreList):
-    '''Function that takes a sentence, finds keywords, and calculates a score based on keywords in sentence'''
-    # score is an ordered list of the happiness score value of each tweet
+
+def calculateTweetScore(sentence_list, keyword_list, coordinates, region_score_list):
+    """Function that takes a sentence, finds keywords, and calculates a score based on keywords in sentence"""
     # sentenceList is a list of sentences (which are lists of words) to compute
     # keywordList is a list of tuples containing the keyword to find (0) and the score of that word
     # lists are passed by reference therefore they do not need to be returned
 
+    countKeywordTweets = [0, 0, 0, 0]  # will count the number of keyword tweets in the list.
+    countTweets = [0, 0, 0, 0]
 
-    countKeywordTweets = [0,0,0,0] # will count the number of keyword tweets in the list.
-    countTweets = [0,0,0,0]
-
-    for tweet in range(len(sentenceList)):
-        #print(coordinates[tweet])
-        #print("Tweet: ", tweet)
+    for tweet in range(len(sentence_list)):
         # iterating through tweets
-        tweetRegion = tweetLocation(coordinates[tweet], countTweets)
+        tweetRegion = tweetLocation(coordinates[tweet], countTweets)  # determining tweet region
         if tweetRegion == 1 or tweetRegion == 2 or tweetRegion == 3 or tweetRegion == 0:
-            scoreTemp=0.0   # will add the score value of each keyword found
-            countKeywords=0 # will count the number of keywords found in the tweet
-            #print(tweet, coordinates[tweet], tweetRegion)
-            for keyword in keywordList:
+            # ensuring that only tweets within regions are processed
+            scoreTemp = 0.0  # will add the score value of each keyword found
+            countKeywords = 0  # will count the number of keywords found in the tweet
+            for keyword in keyword_list:
                 # iterating through each possible keyword in the list to check if it is a tweet
-                #print("Keyword: ", keyword)
-                for word in sentenceList[tweet]:
-                    #print("Word in tweet: ", word)
+                for word in sentence_list[tweet]:
                     # iterating through words in tweets
                     if keyword[0] == word:
                         keyword[1] = int(keyword[1])
                         scoreTemp = scoreTemp + keyword[1]
-                        #print(tweet, keyword[0], scoreTemp)
-                        countKeywords+=1
+                        countKeywords += 1
             if countKeywords != 0:
                 # in order to avoid ZeroDivisionError
-                scoreTemp = scoreTemp/countKeywords
+                scoreTemp = scoreTemp / countKeywords
                 # taking average of sentiment value score divided by number of keywords found in tweet
                 tweetRegion = tweetLocation(coordinates[tweet], countKeywordTweets)
-                #print(scoreTemp)
+                # adding happiness scores to each region in order to calculate them later
                 if tweetRegion == 0:
-                    regionScoreList[0] += scoreTemp
+                    region_score_list[0] += scoreTemp
                 elif tweetRegion == 1:
-                    regionScoreList[1] += scoreTemp
+                    region_score_list[1] += scoreTemp
                 elif tweetRegion == 2:
-                    regionScoreList[2] += scoreTemp
+                    region_score_list[2] += scoreTemp
                 elif tweetRegion == 3:
-                    regionScoreList[3] += scoreTemp
-                #score.append(scoreTemp)
-                #print(scoreTemp)
+                    region_score_list[3] += scoreTemp
+    return (countKeywordTweets)
 
-    #print(countKeywordTweets)
-    return(countKeywordTweets)
 
 def tweetLocation(stringCoordinates, regionCount):
-    '''function that determines in which region a set of coordinates is based on constants above'''
+    """function that determines in which region a set of coordinates is based on constants above, and counts entries
+    for each region"""
 
     coordinates = []
 
     for item in stringCoordinates:
         coordinates.append(float(item))
 
-    #print(coordinates)
+    # print(coordinates)
     if P1[0] >= coordinates[0] >= P2[0]:
         # determining if point is within latitude
         if P1[1] >= coordinates[1] > P3[1]:
